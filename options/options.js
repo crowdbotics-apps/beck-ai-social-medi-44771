2f68174bc01ca3436847f4e61f052efd62790cca
Crowdbotics <team@crowdbotics.com>
2023-11-27 17:42:15 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "beck-ai-social-medi-44771",
+  url: "https://beck-ai-social-medi-44771.botics.co",
+  api: "https://beck-ai-social-medi-44771.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
