5c864e60300291844123d8b5ae6f2fe2e9584617
Crowdbotics <engineering@crowdbotics.com>
2023-11-30 20:25:46 +0000

    Update store.

--- store/beckaisocialmediAPI/api.js
+++ store/beckaisocialmediAPI/api.js
@@ -1,120 +1,108 @@
-import axios from "axios";
+import axios from "axios"
 const beckaisocialmediAPI = axios.create({
   baseURL: "https://beck-ai-social-medi-44771.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return beckaisocialmediAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return beckaisocialmediAPI.post(`/api/v1/login/`, payload);
+  return beckaisocialmediAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return beckaisocialmediAPI.post(`/api/v1/signup/`, payload);
+  return beckaisocialmediAPI.post(`/api/v1/signup/`, payload)
 }
-
 function modules_privacy_policy_list(payload) {
-  return beckaisocialmediAPI.get(`/modules/privacy-policy/`);
+  return beckaisocialmediAPI.get(`/modules/privacy-policy/`)
 }
-
 function modules_privacy_policy_create(payload) {
-  return beckaisocialmediAPI.post(`/modules/privacy-policy/`, payload);
+  return beckaisocialmediAPI.post(`/modules/privacy-policy/`, payload)
 }
-
 function modules_privacy_policy_retrieve(payload) {
-  return beckaisocialmediAPI.get(`/modules/privacy-policy/${payload.id}/`);
+  return beckaisocialmediAPI.get(`/modules/privacy-policy/${payload.id}/`)
 }
-
 function modules_privacy_policy_update(payload) {
-  return beckaisocialmediAPI.put(`/modules/privacy-policy/${payload.id}/`, payload);
+  return beckaisocialmediAPI.put(
+    `/modules/privacy-policy/${payload.id}/`,
+    payload
+  )
 }
-
 function modules_privacy_policy_partial_update(payload) {
-  return beckaisocialmediAPI.patch(`/modules/privacy-policy/${payload.id}/`, payload);
+  return beckaisocialmediAPI.patch(
+    `/modules/privacy-policy/${payload.id}/`,
+    payload
+  )
 }
-
 function modules_privacy_policy_destroy(payload) {
-  return beckaisocialmediAPI.delete(`/modules/privacy-policy/${payload.id}/`);
+  return beckaisocialmediAPI.delete(`/modules/privacy-policy/${payload.id}/`)
 }
-
 function modules_terms_and_conditions_list(payload) {
-  return beckaisocialmediAPI.get(`/modules/terms-and-conditions/`);
+  return beckaisocialmediAPI.get(`/modules/terms-and-conditions/`)
 }
-
 function modules_terms_and_conditions_create(payload) {
-  return beckaisocialmediAPI.post(`/modules/terms-and-conditions/`, payload);
+  return beckaisocialmediAPI.post(`/modules/terms-and-conditions/`, payload)
 }
-
 function modules_terms_and_conditions_retrieve(payload) {
-  return beckaisocialmediAPI.get(`/modules/terms-and-conditions/${payload.id}/`);
+  return beckaisocialmediAPI.get(`/modules/terms-and-conditions/${payload.id}/`)
 }
-
 function modules_terms_and_conditions_update(payload) {
-  return beckaisocialmediAPI.put(`/modules/terms-and-conditions/${payload.id}/`, payload);
+  return beckaisocialmediAPI.put(
+    `/modules/terms-and-conditions/${payload.id}/`,
+    payload
+  )
 }
-
 function modules_terms_and_conditions_partial_update(payload) {
-  return beckaisocialmediAPI.patch(`/modules/terms-and-conditions/${payload.id}/`, payload);
+  return beckaisocialmediAPI.patch(
+    `/modules/terms-and-conditions/${payload.id}/`,
+    payload
+  )
 }
-
 function modules_terms_and_conditions_destroy(payload) {
-  return beckaisocialmediAPI.delete(`/modules/terms-and-conditions/${payload.id}/`);
+  return beckaisocialmediAPI.delete(
+    `/modules/terms-and-conditions/${payload.id}/`
+  )
 }
-
 function rest_auth_login_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/login/`, payload);
+  return beckaisocialmediAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/logout/`);
+  return beckaisocialmediAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/password/change/`, payload);
+  return beckaisocialmediAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/password/reset/`, payload);
+  return beckaisocialmediAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return beckaisocialmediAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/registration/`, payload);
+  return beckaisocialmediAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_resend_email_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/registration/resend-email/`, payload);
+  return beckaisocialmediAPI.post(
+    `/rest-auth/registration/resend-email/`,
+    payload
+  )
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return beckaisocialmediAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return beckaisocialmediAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload
+  )
 }
-
 function rest_auth_user_retrieve(payload) {
-  return beckaisocialmediAPI.get(`/rest-auth/user/`);
+  return beckaisocialmediAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return beckaisocialmediAPI.put(`/rest-auth/user/`, payload);
+  return beckaisocialmediAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return beckaisocialmediAPI.patch(`/rest-auth/user/`, payload);
+  return beckaisocialmediAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -142,4 +130,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
